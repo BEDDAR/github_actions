5a082e5d6a24f0bc03d0403556fc2d66c0243266
Rebiha BEDDAR <beddarrebiha@gmail.com>
2024-03-07 10:17:36 +0100

    ajout de endpoint get

--- backend.js
+++ backend.js
@@ -3,6 +3,10 @@ const port =5012
 
 const app=express()
 
+app.get('/', (req, res) => {
+    res.send("GET Request Called")
+    })
+
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`)
 })
